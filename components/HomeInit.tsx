be4a952e6ae5dc8707b13e1f121e0aa92df0982e
Vinicio Chaves <47008512+viniciochavess@users.noreply.github.com>
2024-09-19 22:51:48 -0300

    atualizando page

--- components/HomeInit.tsx
+++ components/HomeInit.tsx
@@ -1,31 +1,30 @@
-"use client"
+"use client";
 import { RocketLaunch } from "@phosphor-icons/react";
-import classes from './HomeInit.module.css'
+import classes from "./HomeInit.module.css";
 import { Moon } from "./Moon";
-
-
+import Link from "next/link";
 
 export default function HomeInit() {
   return (
     <>
-  
-      <Moon/>
+      <Moon />
       <div className={classes.containerProfileMain}>
-      <div className={classes.profileMain}>
-        <p className={classes.textOne} >Olá, Eu Sou Vinicio Chaves</p>
-        <h1 className={classes.textTwo} >Software Developer</h1>
-        <span className={classes.textThree}>Future Software Architecture</span>
+        <div className={classes.profileMain}>
+          <p className={classes.textOne}>Olá, Eu Sou Vinicio Chaves</p>
+          <h1 className={classes.textTwo}>Software Developer</h1>
+          <span className={classes.textThree}>
+            Future Software Architecture
+          </span>
+        </div>
+        <div className={classes.containerButton}>
+          <Link className={classes.link} href="/profile">
+            <button className={classes.button}>
+              EXPLORAR
+              <RocketLaunch />
+            </button>
+          </Link>
+        </div>
       </div>
-      <div className={classes.containerButton}>
-        <a className={classes.link}  href="/profile">
-        <button className={classes.button}>EXPLORAR
-        <RocketLaunch/>
-        </button>
-        </a>
-      </div>
-    </div>
-
-     
     </>
-  )
+  );
 }
